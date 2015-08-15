96c08e5e7d0c5f43080e3b85ad978d81ab1eea2c
N1Vo <farcinkiewicz.dom@gmail.com>
2015-08-15 03:19:53 +0100

    Update l33tCho.lua

--- l33tCho.lua
+++ l33tCho.lua
@@ -1,10 +1,16 @@
 Config = scriptConfig("l33tCho", "l33tCho")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
 KSConfig = scriptConfig("KS", "Killsteal:")
 KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, false)
+DrawingsConfig = scriptConfig("Drawings", "Drawings:")
+DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
+
+
+
+
+
 
 
 myIAC = IAC()
@@ -13,6 +19,8 @@ myIAC = IAC()
 OnLoop(function(myHero)
 
 Killsteal()
+
+Drawings()
 
 
 if IWalkConfig.Combo then
@@ -24,10 +32,10 @@ if IWalkConfig.Combo then
    local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,250,GetCastRange(myHero,_Q),210,false,true)
    
    if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
-CastSkillShot(_Q,QPred.PredPos.x,QPred.QredPos.y,QPred.PredPos.z)
+CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 end
 if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and Config.W then
-CastSkillShot(_W,WPred.PredPos.x,WPred.WredPos.y,WPred.PredPos.z)
+CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 end
 end
 end
@@ -39,4 +47,9 @@ function Killsteal()
 CastTargetSpell(enemy, _R)
 end
 end
+end
+
+function Drawings()
+myHeroPos = GetOrigin(myHero)
+if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
 end
